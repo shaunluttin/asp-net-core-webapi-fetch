37dd193b6c911113c61eb73ca03b27a85f0cb042
Shaun Luttin <shaunluttin@gmail.com>
2018-08-30 20:25:08 -0700

    Initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace temp01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public class MyModel 
+        {
+            public int Id { get; set; }
+            public DateTime FromDate { get; set; }
+        }
+
+        // PUT api/values/
+        [HttpPut()]
+        public ActionResult Put([FromBody] MyModel value)
+        {
+            Console.WriteLine("==================");
+            return new JsonResult(value);
+        }
+
+    }
+}
